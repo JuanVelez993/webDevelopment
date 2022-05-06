aebdd7f265874d0e49d08f24cdcb007a764af7d6
Juan Pablo Velez <juan.velez993@gmail.com>
2022-05-06 00:55:34 -0500

    ADD---textdecoration when checked and button disabled if checked

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -157,12 +157,12 @@ const Category = () => {
               {category.listOfTasks ? category.listOfTasks.map(task => {
                 return <tr key={task.id}>
                   <td>{task.id}</td>
-                  <td>{task.taskName}</td>
+                  <td style={task.done ? { textDecoration: 'line-through' } : {}}>{task.taskName}</td>
                   <td>
                     <input onChange={(e) => onCheckBox(e,task)} type="checkbox" checked={task.done} />
                   </td>
                   <td><button className="btn" onClick={(e) => onDeleteTask(task)}>X </button></td>
-                  <td><button className="btn" > Edit Task </button></td> 
+                  <td><button className="btn" disabled={task.done}> Edit Task </button></td> 
                 </tr>
               }) : ""}
         
